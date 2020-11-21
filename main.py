6af5858c62cd070bbfc5376d7bd5f451bcdad4e7
Nour A Alghamdi <65490460+namacoconut@users.noreply.github.com>
2020-11-21 19:25:43 +0000

    1st calss

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-class 
+class Student:
+  def __init__(self, name , age, grade):
+    self.name = name
+    self.age = age
+    self.grade = grade
+
+  def get_grade(self):
+    return self.grade
+
+class Course:
+  def __init__(self, name, max_students):
+    
